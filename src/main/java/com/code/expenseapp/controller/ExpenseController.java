39917acff0823636970caa251c548a8cc5d38d93
epanlauw <epanlauw6@gmail.com>
2022-11-10 17:28:09 +0700

    Add method read expense by id

--- src/main/java/com/code/expenseapp/controller/ExpenseController.java
+++ src/main/java/com/code/expenseapp/controller/ExpenseController.java
@@ -3,8 +3,7 @@ package com.code.expenseapp.controller;
 import com.code.expenseapp.entity.Expense;
 import com.code.expenseapp.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +16,15 @@ public class ExpenseController {
     @GetMapping("/expenses")
     public List<Expense> getAllExpenses() {
         return expenseService.getAllExpenses();
+    }
+
+    @GetMapping("/expenses/{id}")
+    public Expense getExpenseById(@PathVariable Long id) {
+        return expenseService.getExpenseById(id);
+    }
+
+    @DeleteMapping("/expenses")
+    public String deleteExpenseById(@RequestParam Long id) {
+        return "Delete the expense object by id  it's id : " + id;
     }
 }
