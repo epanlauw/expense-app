d1859bbd4b2f026ddd577eb054c4f581ece0e000
epanlauw <epanlauw6@gmail.com>
2022-11-14 14:06:32 +0700

    Add Filter by Category, Name(Searching) and Date

--- src/main/java/com/code/expenseapp/repository/ExpenseRepository.java
+++ src/main/java/com/code/expenseapp/repository/ExpenseRepository.java
@@ -1,9 +1,22 @@
 package com.code.expenseapp.repository;
 
 import com.code.expenseapp.entity.Expense;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    //SELECT FROM * tbl_expenses WHERE category=?
+    Page<Expense> findByCategory(String category, Pageable page);
+
+    //SELECT * FROM tbl_expenses WHERE name LIKE '%keyword%'
+    Page<Expense> findByNameContaining(String keyword, Pageable page);
+
+    //SELECT * FROM tbl_expenses WHERE date BETWEEN 'startDate' AND 'endDate'
+    Page<Expense> findByDateBetween(Date startDate, Date endDate, Pageable page);
 }
--- src/main/java/com/code/expenseapp/service/ExpenseServiceImpl.java
+++ src/main/java/com/code/expenseapp/service/ExpenseServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -53,5 +55,28 @@ public class ExpenseServiceImpl implements ExpenseService{
         existingExpense.setAmount(expense.getAmount() != null ? expense.getAmount() : existingExpense.getAmount());
 
         return expenseRepo.save(existingExpense);
+    }
+
+    @Override
+    public List<Expense> readByCategory(String category, Pageable page) {
+        return expenseRepo.findByCategory(category, page).toList();
+    }
+
+    @Override
+    public List<Expense> readByName(String keyword, Pageable page) {
+        return expenseRepo.findByNameContaining(keyword, page).toList();
+    }
+
+    @Override
+    public List<Expense> readByDate(Date startDate, Date endDate, Pageable page) {
+        if(startDate == null) {
+            startDate = new Date(0);
+        }
+
+        if(endDate == null) {
+            endDate = new Date(System.currentTimeMillis());
+        }
+
+        return expenseRepo.findByDateBetween(startDate, endDate, page).toList();
     }
 }
